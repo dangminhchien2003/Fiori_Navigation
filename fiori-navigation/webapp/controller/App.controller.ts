946008344738489555b068a252b92384489f41bf
Đặng Minh Chiến <Dangminhchien295@gmail.com>
2025-09-26 16:34:58 +0700

    stack 16 17

--- fiori-navigation/webapp/controller/App.controller.ts
+++ fiori-navigation/webapp/controller/App.controller.ts
@@ -1,12 +1,34 @@
-import Controller from "sap/ui/core/mvc/Controller";
+import Log from "sap/base/Log";
+import Base from "./Base.controller";
 
 /**
  * @namespace webapp.controller
  */
-export default class App extends Controller {
+export default class App extends Base {
+  /*eslint-disable @typescript-eslint/no-empty-function*/
+  public onInit(): void {
+    // This is ONLY for being used within the tutorial.
+    // The default log level of the current running environment may be higher than INFO,
+    // in order to see the debug info in the console, the log level needs to be explicitly
+    // set to INFO here.
+    // But for application development, the log level doesn't need to be set again in the code.
+    Log.setLevel(Log.Level.INFO);
 
-    /*eslint-disable @typescript-eslint/no-empty-function*/
-    public onInit(): void {
+    const oRouter = this.getRouter();
 
-    }
+    oRouter.attachBypassed(function (oEvent: any) {
+      const sHash = oEvent.getParameter("hash");
+      // do something here, i.e. send logging data to the backend for analysis
+      // telling what resource the user tried to access...
+      Log.info("Sorry, but the hash '" + sHash + "' is invalid.", "The resource was not found.");
+    });
+
+    oRouter.attachRouteMatched(function (oEvent: any) {
+      const sRouteName = oEvent.getParameter("name");
+      
+      // do something, i.e. send usage statistics to back end
+      // in order to improve our app and the user experience (Build-Measure-Learn cycle)
+      Log.info("User accessed route " + sRouteName + ", timestamp = " + Date.now());
+    });
+  }
 }
